9be322341ba6b60ff7ca8f3cc64aed6c2e272d40
Carbonell-Castillo <brucecastillo1204@gmail.com>
2025-04-03 22:16:48 -0600

    Arrays

---
+++ src/Array.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bcastillo
+ */
+public class Array {
+    public static void main(String[] args) {
+        int edad;
+        
+        edad = 10;
+        
+        edad = 12;
+        
+        
+                
+            
+    }
+   
+}
